5620ed44bd852cc2056a9fd3433183bd8e47a544
Will Riley <will@hflw.org>
2011-04-09 21:54:54 -0400

    finished user list widget/room status widget

--- answerparty/static/dojotoolkit/party/widget/RoomStatus.js
+++ answerparty/static/dojotoolkit/party/widget/RoomStatus.js
@@ -1,6 +1,34 @@
 define("party/widget/RoomStatus", ["dojo", "party", "dijit/_Widget", "dijit/_Templated"], function(dojo, party) {
 
     dojo.declare("party.widget.RoomStatus", [dijit._Widget, dijit._Templated], {
+        templateString: "<ul class='partyRoomStatus'></ul>",
+        currentUser: "",
+
+        addUser: function(name){
+            var li = dojo.create("li", {innerHTML: name});
+            this.domNode.appendChild(li);
+        },
+        clearList: function(){
+            this.domNode.innerHTML = "";
+        },
+
+        markActiveUser: function(name){
+            dojo.query("*", this.domNode).forEach(function(node){
+                if(node.innerHTML == name){
+                    dojo.addClass(node, "active");
+                }else{
+                    dojo.removeClass(node, "active");
+                }
+            });
+        },
+
+        updateList: function(list, nameActive){
+            this.clearList();
+            for(var i in list){
+                this.addUser(list[i]);
+            }
+            this.markActiveUser(nameActive);
+        }
         
     });
 
